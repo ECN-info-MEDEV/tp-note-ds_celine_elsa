260c8f85d8c9c38e46d381957230c67b2f789171
ElsaArr <elsa.arrive.34@gmail.com>
2024-01-18 09:44:42 +0100

    Début implémentation grille

--- src/main/java/centrale/medev/tp_note_medev_celine_elsa/Grille.java
+++ src/main/java/centrale/medev/tp_note_medev_celine_elsa/Grille.java
@@ -19,4 +19,35 @@ public class Grille {
     private String[][] grilleOpposant;
     
     
+    
+    public void afficheGrilleJoueur() {
+        grilleJoueur = new String[longueur][largeur];
+        
+        Point2D origine = new Point2D(0,0);
+        
+        for (int i=0; i<=longueur;i++){
+            for (int k=0; k<=largeur;k++){
+                    grilleJoueur[i][k] = "x";
+              
+        }
+        
+        for (Bateau perso : listePersonnages) {
+            Point2D posP =perso.getPos();
+            if (centre.distance(posP) <= distMaxDeplace) {
+                plateauVisible[posP.getX() - origine.getX()][posP.getY() - origine.getY()] = "P";
+             }
+        }
+        
+        }
+        for (int i=distMaxDeplace*2 ; i>0;i--){
+            System.out.println();
+            for (int k=0; k< 2*distMaxDeplace;k++){
+                System.out.print(" "+(plateauVisible[k][i])+" ");
+            }
+            
+        }
+        System.out.println("");
+
+    }
+
 }
